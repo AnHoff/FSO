e22747aeca525aa72c6ca53bd7ec310115936a29
Ana Hoffmann <ana.luiza.hoff@gmail.com>
2023-10-19 15:39:28 -0300

    Ajusta zumbi + time

--- Trabalho 1/time.c
+++ Trabalho 1/time.c
@@ -1,41 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/time.h>
 #include <sys/wait.h>
-#include <string.h>
 #include <errno.h>
 
-int main() {
-    char cmd[256], arg[256];
-    double total_time = 0;
+void execute_command(const char *path, const char *argument, double *total_time) {
+    struct timeval start, end;
+    double elapsed_time;
 
-    while (scanf("%s %s", cmd, arg) == 2) {
-        struct timeval start_time, end_time;
-        gettimeofday(&start_time, NULL);
+    gettimeofday(&start, NULL);
 
-        pid_t child_pid = fork();
-        if (child_pid == -1) {
-            perror("Fork error");
-            exit(EXIT_FAILURE);
-        }
-        if (child_pid == 0) {
-            execlp(cmd, cmd, arg, NULL);
-            perror("Error");
-            exit(EXIT_FAILURE);
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        perror("Erro ao criar um processo filho");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        execl(path, path, argument, (char *)NULL);
+        perror("Erro ao executar o comando");
+        exit(EXIT_FAILURE);
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+
+        gettimeofday(&end, NULL);
+        elapsed_time = (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_usec - start.tv_usec) / 1000000.0;
+
+        if (WIFEXITED(status)) {
+            printf("> Demorou %.1f segundos, retornou %d\n", elapsed_time, WEXITSTATUS(status));
         } else {
-            int status;
-            waitpid(child_pid, &status, 0);
-            gettimeofday(&end_time, NULL);
-            double elapsed_time = (double)(end_time.tv_sec - start_time.tv_sec) +
-                (double)(end_time.tv_usec - start_time.tv_usec) / 1000000;
-            total_time += elapsed_time;
-            if (WIFEXITED(status)) {
-                printf("> Demorou %.1f segundos, retornou %d\n", elapsed_time, WEXITSTATUS(status));
-            } else {
-                printf("> Erro: %s\n", strerror(errno));
-            }
+            printf("> Erro: %s\n", strerror(errno));
         }
+
+        *total_time += elapsed_time;
+    }
+}
+
+int main() {
+    char line[512];
+    char path[256];
+    char argument[256];
+    double total_time = 0.0;
+
+    while (fgets(line, sizeof(line), stdin)) {
+        if (sscanf(line, "%s %s", path, argument) != 2) {
+            fprintf(stderr, "Erro na entrada: Formato inválido\n");
+            continue;
+        }
+
+        execute_command(path, argument, &total_time);
     }
 
     printf(">> O tempo total foi de %.1f segundos\n", total_time);
--- Trabalho 1/zumbi.c
+++ Trabalho 1/zumbi.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <signal.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
-pid_t zombie_pid = -1;
 int signal_count = 0;
+pid_t zombie_pid = -1;
 
 void signal_handler(int signum) {
-    if (zombie_pid == -1) {
+    signal_count++;
+
+    if (signal_count == 1) {
         zombie_pid = fork();
-        if (zombie_pid == 0) {  // processo filho, não faz nada
+        if (zombie_pid == 0) {
             while (1) {
-                pause();
+                sleep(1);
             }
         }
-    } else {
-        kill(zombie_pid, SIGTERM);
-        zombie_pid = -1;
-        signal_count++;
-    }
-
-    if (signal_count >= 3) {
-        if (zombie_pid != -1) {
+    } else if (signal_count == 2) {
+        if (zombie_pid > 0) {
             kill(zombie_pid, SIGTERM);
+            waitpid(zombie_pid, NULL, 0);
+        }
+    } else if (signal_count == 3) {
+        if (zombie_pid > 0) {
+            kill(zombie_pid, SIGTERM);
+            waitpid(zombie_pid, NULL, 0);
         }
         exit(0);
     }
